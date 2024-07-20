16a81a011750690820b6a9155aa84f6f392edc69
P541M <videna.psalmeleazar@gmail.com>
2024-07-19 22:46:07 -0400

    refactor: got rid of silyl vote.css

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Vote.css"; // Import the CSS file
 
 const Vote = () => {
   const [hasVoted, setHasVoted] = useState(false);
